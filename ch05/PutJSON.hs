5db3641b0b7a84d1f88ec3f2a1dac98384557f11
Manas Apte <manasapte@gmail.com>
2015-04-01 14:51:32 +0530

    changing putjson

--- ch05/PutJSON.hs
+++ ch05/PutJSON.hs
@@ -1,4 +1,4 @@
-module PutJSON where
+module PutJson where
 
 import Data.List (intercalate)
 import SimpleJSON
@@ -11,11 +11,14 @@ renderJValue (JBool True)  = "true"
 renderJValue (JBool False) = "false"
 renderJValue JNull         = "null"
 
-renderJValue (JObject o) = "{" ++ pairs o ++ "}"
-  where pairs [] = ""
+renderJValue (JObject O) = "{" ++ pairs o ++ "}"
+    where pairs [] = ""
           pairs ps = intercalate ", " (map renderPair ps)
-                  renderPair (k,v)   = show k ++ ": " ++ renderJValue v
+          renderPair (k,v) = show k ++ ": " ++ renderJValue v
 
-                  renderJValue (JArray a) = "[" ++ values a ++ "]"
-                    where values [] = ""
-                            values vs = intercalate ", " (map renderJValue vs)
+renderJValue (JArray a) = "[" ++ values a ++ "]" 
+    where values [] = ""
+          values vs = intercalate ", " (map renderJValue vs)
+
+putJValue :: JValue => IO ()
+putJValue v = ptStrLn (renderJValue v)
